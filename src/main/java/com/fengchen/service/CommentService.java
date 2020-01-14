8fb1614c93971465d582f49c7990401ddd78d983
fengchen <chen.feng1@husky.neu.edu>
2020-01-13 16:00:13 -0800

    update

---
+++ src/main/java/com/fengchen/service/CommentService.java
@@ -0,0 +1,13 @@
+package com.fengchen.service;
+
+import com.fengchen.po.Comment;
+
+import java.util.List;
+
+
+public interface CommentService {
+
+    List<Comment> listCommentByBlogId(Long blogId);
+
+    Comment saveComment(Comment comment);
+}
